7109999b4a6855d8e12356592923eab457c2b25a
polijn <leander@xcode.mac>
2024-12-02 13:57:18 +0100

    changed to json call

--- src/routes/api/getTagsFeed/+server.ts
+++ src/routes/api/getTagsFeed/+server.ts
@@ -1,22 +1,13 @@
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 
-// Given tags
-// Return a JSON of FeedViewPost
-export const GET: RequestHandler = async ({ url, locals }) => {
+export const POST: RequestHandler = async ({ request, locals }) => {
   const agent = locals.agent;
   if (!agent) {
     return error(401, "Unauthorized API call");
+
   }
-
-  const queryParams = url.searchParams;
-  const q = queryParams.get("q");
-  const limit = Number(queryParams.get("limit")) || 10;
-
-  if (!q) {
-    return error(500, "q not given");
-  }
-
-  const { data } = await agent.app.bsky.feed.searchPosts({ q, limit });
+  const body = await request.json()
+  const { data } = await agent.app.bsky.feed.searchPosts(body);
   const transformedData = {
     feed: data.posts.map(post => ({ post }))
   };
